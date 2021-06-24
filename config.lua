1f17cf166e12a2d16a195474f42447190ed139e9
zThundy <antonioantonelli7@gmail.com>
2021-06-24 20:17:13 +0200

    added randomId function to tutorial

--- config.lua
+++ config.lua
@@ -62,4 +62,14 @@ Config.WhitelistedJobs = {
         local street = street1 .. ", " .. zone
         return street
     end
+
+    FUNCTION FOR GENERATING RANDOM ID (clientside)
+
+    function randomId()
+        math.randomseed(GetCloudTimeAsInt())
+		return string.gsub("xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx", "[xy]", function(c)
+			local v = (c == "x") and math.random(0, 0xf) or math.random(8, 0xb)
+			return string.format("%x", v)
+		end)
+    end
 ]]
